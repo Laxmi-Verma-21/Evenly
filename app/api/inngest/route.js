586abaf1949d56b49fb29782e6798484152e6d54
Laxmi-Verma-21 <laxmiverma200421@gmail.com>
2025-11-05 14:27:05 +0530

    sending emails using resend

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,13 +1,8 @@
-import { inngest } from "@/lib/inngest/client";
-import { helloWorld } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
+import { inngest } from "@/lib/inngest/client";
+import { paymentReminders } from "@/lib/inngest/payment-reminders";
 
-
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-    /* your functions will be passed here later! */
-    helloWorld,
-  ],
+  functions: [paymentReminders],
 });
